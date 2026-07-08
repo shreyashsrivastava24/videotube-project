33a025731e94135d4c7fe22d6b187e9c36fa2a22
Shreyash Srivastava <shreyashsrivastava010@gmail.com>
2026-07-08 21:20:42 +0530

    API URL change

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://videotube-project-backend.onrender.com/api/v1";
